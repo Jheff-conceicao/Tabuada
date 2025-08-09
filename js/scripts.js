1e248badbc0c985fc8231b4bd576fa0b6eb038f1
Jeferson da Conceição <conceicao.jeferson@gmail.com>
2025-08-08 22:02:37 -0300

    final do projeto

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,43 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 
-// Funções
+const multiplicationTitle = document.querySelector("#multiplication-title span");
 
+const multiplicationTable = document.querySelector("#multiplication-operations");
+
+// Funções
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for(i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i;
+
+        const template = `<div class="row">
+            <div class="operation">${number} x ${i} = </div>
+            <div class="result">${result}</div>
+        </div>`;
+
+     const parser = new DOMParser()
+
+     const htmlTemplate = parser.parseFromString(template, "text/html")
+
+     const row = htmlTemplate.querySelector(".row")
+
+     multiplicationTable.appendChild(row);
+    }
+
+    multiplicationTitle.innerText = number;
+
+};
 // Eventos
 multiplicationForm.addEventListener("submit", (e) => {
- e.preventDefault();
+  e.preventDefault();
 
-    const multiplicationNumber = numberInput.value;
+  const multiplicationNumber = numberInput.value;
 
-    const multiplicatorNumber =+multiplicationInput.value;
+  const multiplicatorNumber = +multiplicationInput.value;
 
-if(!multiplicationNumber || !multiplicatorNumber) return;
+  if (!multiplicationNumber || !multiplicatorNumber) return;
 
-    console.log(multiplicationNumber, multiplicationNumber)
-})
+  createTable(multiplicationNumber, multiplicatorNumber);
+});
